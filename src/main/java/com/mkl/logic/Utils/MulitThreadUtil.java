e7bcc0df2c9a369c9c29a3535e4d5aa77f8949f6
mkl <mkl@mikiaholding.dk>
2019-01-11 17:16:03 +0100

    added derby testdb

---
+++ src/main/java/com/mkl/logic/Utils/MulitThreadUtil.java
@@ -0,0 +1,23 @@
+package com.mkl.logic.Utils;
+
+import java.util.ArrayList;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.List;
+
+public class MulitThreadUtil<Edto>
+{
+	/*
+		note: create interface util extends Com,
+		implement Function in multi?
+
+
+	public Edto threadProblem(int amount) {
+		ExecutorService executor = Executors.newFixedThreadPool(10);
+		List<Future<Edto>>  futures = new ArrayList<>();
+		for(int i = 0; i < amount; i++) {
+			Future<Edto> future = executor.submit();
+		}
+	} */
+}
--- src/test/java/com/mkl/AnyObjectResourceTest.java
+++ src/test/java/com/mkl/AnyObjectResourceTest.java
@@ -1,6 +1,11 @@
 package com.mkl;
 
+import com.mkl.data.entities.AnyObject;
 import org.junit.Test;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 
 import static org.junit.Assert.*;
 
@@ -10,6 +15,15 @@ public class AnyObjectResourceTest
     @Test
     public void getIt()
     {
+        EntityManagerFactory emf      = Persistence.createEntityManagerFactory("rest-api-test-pu");
+        EntityManager        em       = emf.createEntityManager();
+        AnyObject            expected = new AnyObject("ok");
 
+        em.getTransaction().begin();
+        em.persist(expected);
+        AnyObject actual = em.find(AnyObject.class, 1);
+        em.getTransaction().commit();
+
+        assertEquals(actual, expected);
     }
 }
