74e4e497d26a4faab7abf6c9d8264af92b0d8d6f
mkl <mkl@mikiaholding.dk>
2019-01-13 15:51:25 +0100

    created imports for test

--- src/test/java/com/mkl/TestObject.java
+++ src/test/java/com/mkl/TestObject.java
@@ -1,20 +1,26 @@
 package com.mkl;
 
 import org.junit.Before;
+import org.junit.Test;
 
+import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 
 public class TestObject
 {
 	private EntityManagerFactory emf;
 	@Before public void initialize() {
-
-
+		persistItem();
 	}
 
 	//add item to param
-	private void persistenItem() {
+	private void persistItem() {
 		this.emf = TestJPAConnector.getEntityManagerFactory();
+	}
+
+	@Test
+	public void checkInitialize() {
+		EntityManager em = emf.createEntityManager();
 	}
 
 }
